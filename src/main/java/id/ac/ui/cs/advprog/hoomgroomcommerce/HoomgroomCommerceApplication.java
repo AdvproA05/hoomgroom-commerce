ae279e21fb3412f5e0d8d41dfd9e24273d72da9f
shirinzarqaa <shirinzarqaa354@gmail.com>
2024-04-07 20:16:15 +0700

    initialized Spring

---
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroomcommerce/HoomgroomCommerceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.hoomgroomcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoomgroomCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoomgroomCommerceApplication.class, args);
+	}
+
+}
