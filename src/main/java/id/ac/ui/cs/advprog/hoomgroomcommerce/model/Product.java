5a12fab1ad1794411d81c0dd4de8591bf2f10dfc
shirinzarqaa <shirinzarqaa354@gmail.com>
2024-04-23 00:22:15 +0700

    [GREEN] Implement Product model.

--- src/main/java/id/ac/ui/cs/advprog/hoomgroomcommerce/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroomcommerce/model/Product.java
@@ -1,4 +1,21 @@
 package id.ac.ui.cs.advprog.hoomgroomcommerce.model;
 
+
+import java.util.UUID;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter @Setter
 public class Product {
+    private UUID productId;
+    private String productName;
+    private String productDescription;
+    private String productImage;
+    private int productQuantity;
+    private Double productPrice;
+    private Double productDiscountPrice;
+
 }
