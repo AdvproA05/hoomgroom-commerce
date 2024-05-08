2e52cc1391edc0f6997578d864c2574037049beb
shirinzarqaa <shirinzarqaa354@gmail.com>
2024-05-08 19:00:48 +0700

    [RED] add skeleton for strategy pattern

---
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroomcommerce/service/ProductSearchStrategy.java
@@ -0,0 +1,8 @@
+package id.ac.ui.cs.advprog.hoomgroomcommerce.service;
+
+import id.ac.ui.cs.advprog.hoomgroomcommerce.model.Product;
+
+import java.util.List;
+public interface ProductSearchStrategy {
+    List<Product> filterFindProduct();
+}
