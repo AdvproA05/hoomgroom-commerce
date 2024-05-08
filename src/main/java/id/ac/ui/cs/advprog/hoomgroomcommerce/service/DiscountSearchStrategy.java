199ff47e3e342381b8d60ecf5a3d3121e8102d01
shirinzarqaa <shirinzarqaa354@gmail.com>
2024-05-08 19:07:14 +0700

    [green] implement Strategy pattern for search product by discount

---
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroomcommerce/service/DiscountSearchStrategy.java
@@ -0,0 +1,26 @@
+package id.ac.ui.cs.advprog.hoomgroomcommerce.service;
+
+import id.ac.ui.cs.advprog.hoomgroomcommerce.model.Product;
+import id.ac.ui.cs.advprog.hoomgroomcommerce.repository.ProductRepository;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class DiscountSearchStrategy implements SearchStrategy {
+    private ProductRepository productRepository;
+
+    public DiscountSearchStrategy(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<Product> filterProducts() {
+        List<Product> allProducts = productRepository.findAll();
+        allProducts.removeIf(product -> product.getProductDiscountPrice() == null || product.getProductDiscountPrice() == 0);
+
+        Collections.sort(allProducts, Comparator.comparingDouble(Product::getProductDiscountPrice).reversed());
+
+        return allProducts;
+    }
+}
