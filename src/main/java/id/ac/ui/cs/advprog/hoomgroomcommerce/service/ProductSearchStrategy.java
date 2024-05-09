d488fb34b7232cd005d7f80dadbc1a41776ac3ac
shirinzarqaa <110030938+shirinzarqaa@users.noreply.github.com>
2024-05-09 23:49:45 +0700

    [REFACTOR] Remove Product Search Strategy Skeleton.

--- src/main/java/id/ac/ui/cs/advprog/hoomgroomcommerce/service/ProductSearchStrategy.java
+++
@@ -1,8 +0,0 @@
-package id.ac.ui.cs.advprog.hoomgroomcommerce.service;
-
-import id.ac.ui.cs.advprog.hoomgroomcommerce.model.Product;
-
-import java.util.List;
-public interface ProductSearchStrategy {
-    List<Product> filterFindProduct();
-}
