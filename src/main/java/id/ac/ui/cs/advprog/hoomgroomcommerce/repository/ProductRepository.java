c72a00186340c81c6769ffb462e6d92c533ffd72
shirinzarqaa <shirinzarqaa354@gmail.com>
2024-05-10 23:41:20 +0700

    [REFACTOR] add annotation.

--- src/main/java/id/ac/ui/cs/advprog/hoomgroomcommerce/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroomcommerce/repository/ProductRepository.java
@@ -2,11 +2,13 @@ package id.ac.ui.cs.advprog.hoomgroomcommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import id.ac.ui.cs.advprog.hoomgroomcommerce.model.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
     ArrayList<Product> findByProductNameContainingIgnoreCase(String keyword);
     ArrayList<Product> findByProductDescriptionContainingIgnoreCase(String keyword);
