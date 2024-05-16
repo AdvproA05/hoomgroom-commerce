d7320c05a9e4d5b30b0730e0207aff17d59ba036
yela <124979875+VirgilliaYeala@users.noreply.github.com>
2024-05-16 12:31:32 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
 	runtimeOnly("org.postgresql:postgresql")
+}
 
 
 tasks.register<Test>("unitTest") {
