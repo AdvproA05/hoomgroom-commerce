28a39beca96cdd83120a3406101f9762663509ce
HanifRifky <114400903+HanifRifky@users.noreply.github.com>
2024-05-23 13:08:39 +0700

    [GREEN] Implementasi test atas penyesuaian sebelumnya

--- src/test/java/id/ac/ui/cs/advprog/hoomgroomcommerce/controller/PengirimanControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/hoomgroomcommerce/controller/PengirimanControllerTest.java
@@ -1,34 +1,56 @@
 package id.ac.ui.cs.advprog.hoomgroomcommerce.controller;
 
+import id.ac.ui.cs.advprog.hoomgroomcommerce.enums.PengirimanState;
 import id.ac.ui.cs.advprog.hoomgroomcommerce.model.Pengiriman;
+import id.ac.ui.cs.advprog.hoomgroomcommerce.model.Transportation;
 import id.ac.ui.cs.advprog.hoomgroomcommerce.service.PengirimanService;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-@WebMvcTest(PengirimanController.class)
-@AutoConfigureMockMvc
-public class PengirimanControllerTest {
+import java.util.Arrays;
+import java.util.List;
 
-    @Autowired
-    private MockMvc mockMvc;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
-    @MockBean
+class PengirimanControllerTest {
+
+    @Mock
     private PengirimanService pengirimanService;
 
-    @Test
-    public void testUpdateStatusPengiriman() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.put("/pengiriman/updateStatus/123/DIKIRIM")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+    @InjectMocks
+    private PengirimanController pengirimanController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
- 
+    @Test
+    void testCreatePengiriman() {
+        Pengiriman pengiriman = new Pengiriman("123");
+        when(pengirimanService.createPengiriman(pengiriman)).thenReturn(pengiriman);
+
+        Pengiriman createdPengiriman = pengirimanController.creatPengiriman(pengiriman);
+
+        assertEquals(pengiriman, createdPengiriman);
+        verify(pengirimanService, times(1)).createPengiriman(pengiriman);
+    }
+
+    @Test
+    void testGetAllPengiriman() {
+        Pengiriman pengiriman1 = new Pengiriman("123");
+        Pengiriman pengiriman2 = new Pengiriman("456");
+        List<Pengiriman> pengirimans = Arrays.asList(pengiriman1, pengiriman2);
+        when(pengirimanService.findAllPengiriman()).thenReturn(pengirimans);
+
+        List<Pengiriman> retrievedPengirimans = pengirimanController.getAllPengiriman();
+
+        assertEquals(pengirimans, retrievedPengirimans);
+        verify(pengirimanService, times(1)).findAllPengiriman();
+    }
 }
--- src/test/java/id/ac/ui/cs/advprog/hoomgroomcommerce/model/PengirimanTest.java
+++ src/test/java/id/ac/ui/cs/advprog/hoomgroomcommerce/model/PengirimanTest.java
@@ -1,13 +1,36 @@
 package id.ac.ui.cs.advprog.hoomgroomcommerce.model;
 
+import id.ac.ui.cs.advprog.hoomgroomcommerce.enums.PengirimanState;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class PengirimanTest {
 
     @Test
-    public void testConstructor() {
-        Pengiriman pengiriman = new Pengiriman();
-        assertNotNull(pengiriman);
+    void testGetPengirimanState() {
+        Pengiriman pengiriman = new Pengiriman("ABC123");
+        assertEquals(PengirimanState.MENUNGGU_VERIFIKASI, pengiriman.getState());
+    }
+
+    @Test
+    void testGetTransportation() {
+        Transportation transportation = new Transportation("Truck");
+        Pengiriman pengiriman = new Pengiriman("ABC123");
+        pengiriman.setTransportasi(transportation);
+        assertEquals("Truck", pengiriman.getTransportasi().getType());
+    }
+
+    @Test
+    void testGetTransportationNull() {
+        Pengiriman pengiriman = new Pengiriman("ABC123");
+        assertNull(pengiriman.getTransportasi());
+    }
+
+    @Test
+    void testGetKodeResi() {
+        Pengiriman pengiriman = new Pengiriman("ABC123");
+        assertEquals("ABC123", pengiriman.getKodeResi());
     }
 }
