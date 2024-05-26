74ba8340f6aa1266f360eda58212290eae5ece2e
g0lgi <epatrickk0505@gmail.com>
2024-05-26 19:07:22 +0700

    hapus dependency springboot security

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,6 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("org.postgresql:postgresql")
-    testImplementation("org.springframework.security:spring-security-test")
     implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("com.h2database:h2")
@@ -51,7 +50,6 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
-    implementation("org.springframework.boot:spring-boot-starter-security")
 }
 
 tasks.register<Test>("unitTest") {
