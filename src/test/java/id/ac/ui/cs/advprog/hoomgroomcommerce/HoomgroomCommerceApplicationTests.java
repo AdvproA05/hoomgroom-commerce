fc9deaf5693441ba7f2ff3b4c1ea1134ef3849b7
Shirin Zarqaa <shirinzarqaa354@gmail.com>
2024-05-26 18:43:33 +0700

    add test hoomgroom commerce

--- src/test/java/id/ac/ui/cs/advprog/hoomgroomcommerce/HoomgroomCommerceApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/hoomgroomcommerce/HoomgroomCommerceApplicationTests.java
@@ -4,5 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HoomgroomCommerceApplicationTests {
+class git {
+    @Test
+    public void contextLoads() {
+    }
 }
